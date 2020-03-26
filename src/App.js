22ea8fd47186c7e0e42f51713639c6e19fb4a147
ebzm00 <49386594+ebzm00@users.noreply.github.com>
2020-03-26 14:03:59 +0900

    apply material UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root :{
+    width : '100%',
+    marginTop : theme.spacing.unit * 3 ,
+    overflowX : "auto"
+  },
+
+  table : {
+    minWidth : 1080
+  }
+})
 
 const customers = [{
   'id':1,
@@ -35,16 +48,27 @@ const customers = [{
 }]
 class App extends Component { 
   render() { 
+    const {classes} = this.props;
     return (
-      <div>
-        <Table>
+      <Paper className = {classes.root}>
+        <Table className = {classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>직업</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>생년월일</TableCell>
+            </TableRow>
+          </TableHead>
          <TableBody>
         {customers.map(c => { return( <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} job = {c.job} gender = {c.gender}/> )})}
          </TableBody>
         </Table>
-      </div>
+      </Paper>
       );
     }
   }
 
-export default App;
+export default withStyles(styles)(App);
